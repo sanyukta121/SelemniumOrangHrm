67cdac352f2def2eb8cb3b02679d3d0b62d9677c
SS <s.sanyukta@gmail.com>
2026-07-12 21:47:06 -0500

    Initial commit

---
+++ src/test/java/Page/LoginPage.java
@@ -0,0 +1,36 @@
+package Page;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    public WebDriver driver;
+
+    private By Username = By.xpath("//input[@placeholder='Username']");
+    private By Password = By.xpath("//input[@placeholder='Password']");
+    private By Login_Btn = By.xpath("//button[normalize-space()='Login']");
+
+
+    Public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        return null;
+
+    }
+
+
+    void enterUserName(String username) {
+        driver.findElement(Username).sendKeys("username");
+
+    }
+
+    void enterPassword(String pwd) {
+        driver.findElement(Password).sendKeys("pwd");
+    }
+    void clickLogin() {
+        driver.findElement(Login_Btn).click();
+
+    }
+}
+
+
